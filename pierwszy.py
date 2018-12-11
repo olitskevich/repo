d7a117940000e9c16dfcc1cc829ae52cc032bd63
Olga Litskevich <olgalickewicz@gmail.com>
2018-12-11 19:16:59 +0100

    Zmiany w skrypcie pierwszy.py Dodalismy info o dzisiejszej dacie i o tym, ze pracujemy z Gitem

--- pierwszy.py
+++ pierwszy.py
@@ -3,3 +3,8 @@
 #To jest nasz pierwszy skrypt Pythona
 
 print("Witaj swiecie. To jest nasz pierwszy skrypt Pythona dzisiaj")
+
+print("Dzis jest 11 grudnia i uczymy sie Gita")
+
+# to drugi komentarz
+
