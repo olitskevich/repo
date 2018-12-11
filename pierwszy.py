28d465472dbca48e8321d999a872aa3bbdf97cd8
Olga Litskevich <olgalickewicz@gmail.com>
2018-12-11 19:51:56 +0100

    Dodalismy funkcje input do skryptu pieerwszy.py

--- pierwszy.py
+++ pierwszy.py
@@ -2,9 +2,13 @@
 
 #To jest nasz pierwszy skrypt Pythona
 
-print("Witaj swiecie. To jest nasz pierwszy skrypt Pythona dzisiaj")
+print("Witaj swiecie. To jest nasz pierwszy skrypt Pythona dzisiaj.")
 
-print("Dzis jest 11 grudnia i uczymy sie Gita")
+print("Dzis jest 11 grudnia i uczymy sie Gita.")
+
+print ("Podaj swoje imie bysmy mogli zapoznac.")
+nazwa = input()
+print("Witaj", nazwa,"! Miło Cie widziec.")
 
 # to drugi komentarz
 
