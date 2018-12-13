ae329fc93dfed675ac3fca030483bbcdb3e51481
Klemens Noga <klemens.noga@gmail.com>
2018-12-13 20:21:58 +0100

    Generowanie konfiliku

--- do_olgi.py
+++ do_olgi.py
@@ -1,8 +1,3 @@
 #!/usr/bin/env python
 
-<<<<<<< HEAD
-print("Hej Olga! Tu Piotr :)")
-=======
-print("Siemka Olga.")
->>>>>>> 6dfe4dfb27265fa82010cab9087d9c152f7b806a
-
+print("Prosze nie bawic sie repozytorium Olgi!!!!!!!!")
