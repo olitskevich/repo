72f80f7ad356bb27308f563552c3da4b2c4ea7e0
Klemens Noga <klemens.noga@gmail.com>
2018-12-13 20:51:03 +0100

    Drugi konflikt

--- do_olgi.py
+++ do_olgi.py
@@ -3,4 +3,4 @@
 print("Prosze nie bawic sie repozytorium Olgi!!!!!!!!")
 print("Tutaj Olga")
 print("Siemka Olga.")
-print("Konflikt rozwiązany")
+print("Konflikt rozwiązany. A jednak nie KLemens. Bedziemy walczyc dalej")
